5e217f922b2f8ea01999dc849b13f98a5f4ca174
allanmuraroto <allan.muraroto@hotmail.com>
2022-08-19 00:13:36 -0300

    preparando status do tamagotchi

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
 /* alert('jacaré com fome'); */
+
+var fome = 100;
+var higiene = 100;
+var sono = 100;
+var diversao = 100;
+var social = 100;
+
+function Loop(time = 2)
+{
+    var total = fome+higiene+sono+social+diversao;
+}
