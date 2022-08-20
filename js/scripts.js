5b6d62044d3fa8c2916cb11b0fadd8e2e7bcc369
allanmuraroto <allan.muraroto@hotmail.com>
2022-08-19 23:58:23 -0300

    Commit do dia 19, nenhuma alteração significante

--- js/scripts.js
+++ js/scripts.js
@@ -8,5 +8,15 @@ var social = 100;
 
 function Loop(time = 2)
 {
-    var total = fome+higiene+sono+social+diversao;
+    var total = fome+higiene+sono+social+diversao+social;
+
+    if(fome <= 0|| higiente <= 0|| sono <= 0|| diversao <= 0|| social <= 0)
+    {
+        document.getElementById('pet').src="img/preto.jpg";
+        document.getElementById('pet').innerHTML = "Você deixou seu pet morrer, atualize a página";
+    }else if(total > 400){
+        document.getElementById('pet').src="img/azul.jpg"
+    }
+
+
 }
