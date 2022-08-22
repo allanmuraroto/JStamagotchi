82d74f8ddf1da30cbdcf3e7fa3c27a276701df71
allanmuraroto <allan.muraroto@hotmail.com>
2022-08-22 00:31:07 -0300

    Tela inicial do Tamagotchi

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-/* alert('jacaré com fome'); */
-
 var fome = 100;
 var higiene = 100;
 var sono = 100;
@@ -10,13 +8,47 @@ function Loop(time = 2)
 {
     var total = fome+higiene+sono+social+diversao+social;
 
-    if(fome <= 0|| higiente <= 0|| sono <= 0|| diversao <= 0|| social <= 0)
+    if(fome <= 0|| higiene <= 0|| sono <= 0|| diversao <= 0|| social <= 0)
     {
         document.getElementById('pet').src="img/preto.jpg";
         document.getElementById('pet').innerHTML = "Você deixou seu pet morrer, atualize a página";
     }else if(total > 400){
         document.getElementById('pet').src="img/azul.jpg"
+    }else if(total > 300){
+        document.getElementById('pet').src="img/verde.jpg"
+    }else if(total > 200){
+        document.getElementById('pet').src="img/cinza.jpg"
+    }else if(total > 100){
+        document.getElementById('pet').src="img/amarelo.jpg"
+    }else if(total > 50){
+        document.getElementById('pet').src="img/vermelho.jpg"
     }
 
 
+    fome = fome - parseInt(time);
+    higiene = higiene - parseInt(time);
+    sono = sono - parseInt(time);
+    diversao = diversao - parseInt(time);
+    social = social - parseInt(time);
+
+    document.getElementById('fome').innerHTML = fome + '%';
+    document.getElementById('higiene').innerHTML = higiene + '%';
+    document.getElementById('sono').innerHTML = sono + '%';
+    document.getElementById('diversao').innerHTML = diversao + '%';
+    document.getElementById('social').innerHTML = social + '%';
+    
 }
+
+function start()
+{
+    var temporizador = setInterval(Loop, 1000)
+}
+
+function Comer()
+{
+    document.getElementById('fome').innerHtml = fome + '%';
+    document.getElementById('fome').style.width = fome + '%';
+}
+
+
+start()
